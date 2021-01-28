18b3a3aaf597e3b6551638ab095e0a7d2edfe075
Omar Farid <omarfarid222@gmail.com>
2021-01-28 04:15:08 +0200

    flash errors

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -42,12 +42,20 @@ router.post('/', validateCampground, catchAsync(async (req, res, next) => {
 // get route to show campground by id  
 router.get('/:id', catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id).populate('reviews');
+    if (!campground) {
+        req.flash('error', 'Cannot find that campground!');
+        return res.redirect('/campgrounds');
+    }
     res.render('campgrounds/show', { campground, title: ` - ${campground.title}` });
 }));
 
 // get route to edit campground by id  
 router.get('/:id/edit', catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id);
+    if (!campground) {
+        req.flash('error', 'Cannot find that campground!');
+        return res.redirect('/campgrounds');
+    }
     res.render('campgrounds/edit', { campground, title: ' - Edit' });
 }));
 
