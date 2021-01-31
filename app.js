7c7d21d6b68b3ed14126bf5dcda4744aa8025f70
Omar Farid <omarfarid222@gmail.com>
2021-02-01 01:08:28 +0200

    Register Route Logic

--- app.js
+++ app.js
@@ -72,12 +72,6 @@ app.use((req, res, next) => {
     next();
 })
 
-app.get('/fakeUser', async (req, res) => {
-    const user = new User({ email: 'omar@gmail.com', username: 'omar' });
-    const newUser = await User.register(user, 'monkey');
-    res.send(newUser);
-});
-
 // importing routes
 app.use('/campgrounds', campgroundRoutes);
 app.use('/campgrounds/:id/reviews', reviewRoutes);
--- routes/users.js
+++ routes/users.js
@@ -8,8 +8,17 @@ router.get('/register', (req, res) => {
 });
 
 router.post('/register', async (req, res) => {
-
-    res.send(req.body);
+    try {
+        const { email, username, password } = req.body;
+        const user = new User({ email, username });
+        await User.register(user, password);
+        console.log(user);
+        req.flash('success', 'Welcome to zaCamp!');
+        res.redirect('/');
+    } catch (error) {
+        req.flash('error', error.message);
+        res.redirect('/register');
+    }
 });
 
 module.exports = router;
