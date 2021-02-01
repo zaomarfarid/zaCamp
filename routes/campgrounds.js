42924aa5114acaa1912165bf3480e9d0ce31b4fd
Omar Farid <omarfarid222@gmail.com>
2021-02-01 14:18:09 +0200

    Adding an Author

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -35,6 +35,7 @@ router.get('/new', isLoggedIn, (req, res) => {
 // post route redirects to the NEW added campground based on its id
 router.post('/', isLoggedIn, validateCampground, catchAsync(async (req, res, next) => {
     const campground = new Campground(req.body.campground);
+    campground.author = req.user._id;
     await campground.save();
     req.flash('success', 'Successfully made a new campground!');
     res.redirect(`/campgrounds/${campground._id}`);
@@ -42,7 +43,7 @@ router.post('/', isLoggedIn, validateCampground, catchAsync(async (req, res, nex
 
 // get route to show campground by id  
 router.get('/:id', catchAsync(async (req, res) => {
-    const campground = await Campground.findById(req.params.id).populate('reviews');
+    const campground = await Campground.findById(req.params.id).populate('reviews').populate('author');
     if (!campground) {
         req.flash('error', 'Cannot find that campground!');
         return res.redirect('/campgrounds');
