e541972e7533a8052f067ff7753f42c068b72d66
Omar Farid <omarfarid222@gmail.com>
2021-02-07 23:21:16 +0200

    Deleting Images Form

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -44,6 +44,7 @@ module.exports.renderEditForm = async (req, res) => {
 
 module.exports.updateCampground = async (req, res) => {
     const { id } = req.params;
+    console.log(req.body);
     const campground = await Campground.findByIdAndUpdate(id, req.body.campground);
     campground.images.push(...(req.files.map(file => ({ url: file.path, filename: file.filename }))));
     await campground.save();
