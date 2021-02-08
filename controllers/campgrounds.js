cc2b96f2e40f211393cbe90d39ae20dc2baebc51
Omar Farid <omarfarid222@gmail.com>
2021-02-08 18:53:10 +0200

    Working With GroJSON

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -17,14 +17,14 @@ module.exports.createCampground = async (req, res) => {
         query: req.body.campground.location,
         limit: 1
     }).send()
-    res.send(geoData.body.features[0].geometry.coordinates)
-    // const campground = new Campground(req.body.campground);
-    // campground.images = req.files.map(file => ({ url: file.path, filename: file.filename }));
-    // campground.author = req.user._id;
-    // await campground.save();
-    // console.log(campground);
-    // req.flash('success', 'Successfully made a new campground!');
-    // res.redirect(`/campgrounds/${campground._id}`);
+    const campground = new Campground(req.body.campground);
+    campground.geometry = geoData.body.features[0].geometry;
+    campground.images = req.files.map(file => ({ url: file.path, filename: file.filename }));
+    campground.author = req.user._id;
+    await campground.save();
+    console.log(campground);
+    req.flash('success', 'Successfully made a new campground!');
+    res.redirect(`/campgrounds/${campground._id}`);
 }
 
 module.exports.showCampground = async (req, res) => {
