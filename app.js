8fb25cc248cf202dab03fe5a120d20fe6bc011a8
Omar Farid <omarfarid222@gmail.com>
2021-02-11 04:13:25 +0200

    Content Security Policy Fun

--- app.js
+++ app.js
@@ -67,7 +67,48 @@ app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(mongoSanitize());
-app.use(helmet({ contentSecurityPolicy: false }));
+app.use(helmet());
+
+const scriptSrcUrls = [
+    "https://api.tiles.mapbox.com",
+    "https://api.mapbox.com",
+    "https://kit.fontawesome.com",
+    "https://cdnjs.cloudflare.com",
+    "https://cdn.jsdelivr.net",
+];
+const styleSrcUrls = [
+    "https://kit-free.fontawesome.com",
+    "https://api.mapbox.com",
+    "https://api.tiles.mapbox.com",
+    "https://fonts.googleapis.com",
+    "https://use.fontawesome.com",
+    "https://cdn.jsdelivr.net",
+];
+const connectSrcUrls = [
+    "https://api.mapbox.com",
+    "https://*.tiles.mapbox.com",
+    "https://events.mapbox.com",
+];
+const imageSrcUrls = [
+    "https://res.cloudinary.com/zaomarfarid/",
+    "https://images.unsplash.com",
+    "https://cdn.onlinewebfonts.com"
+];
+app.use(
+    helmet.contentSecurityPolicy({
+        directives: {
+            defaultSrc: [],
+            connectSrc: ["'self'", ...connectSrcUrls],
+            scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
+            styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+            workerSrc: ["'self'", "blob:"],
+            childSrc: ["blob:"],
+            objectSrc: [],
+            imgSrc: ["'self'", "blob:", "data:", ...imageSrcUrls],
+            fontSrc: ["'self'"],
+        },
+    })
+);
 
 app.use(passport.initialize());
 app.use(passport.session());
